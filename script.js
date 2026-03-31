a36fc35b4568650dd0bb50b2798fc4f1dec3a5d9
Raynnerzzk7 <raynnerzzk7@gmail.com>
2026-03-31 17:55:06 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("form").addEventListener("submit", function(e) {
-    const senha = document.querySelector("input[name='senha']").value;
-
-    if (senha.length < 6) {
-        alert("Senha deve ter no mínimo 6 caracteres!");
-        e.preventDefault();
-    }
-});
